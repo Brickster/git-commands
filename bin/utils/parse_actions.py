1ceafadc042b9efc77d3d9d5fca9aff367369151
Marcus Rosenow <2048345+Brickster@users.noreply.github.com>
2015-04-12 18:08:40 -0500

    Add multi_set action

--- bin/utils/parse_actions.py
+++ bin/utils/parse_actions.py
@@ -6,3 +6,11 @@ def flag_as_value(value):
         def __call__(self, parser, namespace, values, option_string=None):
             setattr(namespace, self.dest, value)
     return FlagAsInt
+
+
+def multi_set(dest1, value1):
+    class MultiSet(argparse.Action):
+        def __call__(self, parser, namespace, values, option_string=None):
+            setattr(namespace, dest1, value1)
+            setattr(namespace, self.dest, values)
+    return MultiSet
