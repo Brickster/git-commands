3d13d8465b4271e407d592f9cab1c3c2a65774df
Marcus Rosenow <2048345+Brickster@users.noreply.github.com>
2015-04-15 08:24:17 -0500

    Extract snapshot into commands module

---
+++ bin/commands/snapshot.py
@@ -0,0 +1,18 @@
+from subprocess import check_output, call
+from utils.messages import info
+
+
+def snapshot(message):
+    '''Create a snapshot of the working directory and index.'''
+
+    status_command = ['git', 'status', '--porcelain']
+    status_output = check_output(status_command).splitlines()
+
+    if len(status_output) > 0:
+        stash_command = ['git', 'stash', 'save', '-u', '--quiet']
+        stash_command = stash_command if message is None else stash_command + [message]
+
+        call(stash_command)
+        call(['git', 'stash', 'apply', '--quiet'])
+    else:
+        info("No local changes to save. No snapshot created.")
