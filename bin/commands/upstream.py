9575de60c04e3a0b057019df0daadaa79f01bffc
Marcus Rosenow <2048345+Brickster@users.noreply.github.com>
2015-04-15 08:00:25 -0500

    Extract upstream into commands module

---
+++ bin/commands/upstream.py
@@ -0,0 +1,17 @@
+import re
+
+from subprocess import check_output
+
+
+def upstream(include_remote=False):
+    """Get the upstream branch of the current branch."""
+
+    branch_info = check_output(['git', 'status', '--porcelain', '--branch']).splitlines()[0]
+    regex = "^##\s[-_a-zA-Z0-9]+\.{3}([-_a-zA-Z0-9]+/([-_a-zA-Z0-9]+))(?:\s\[.*\])?$"
+    match = re.match(regex, branch_info)
+
+    upstream_info = ''
+    if match is not None:
+        upstream_info = match.group(1) if include_remote else match.group(2)
+
+    return upstream_info
