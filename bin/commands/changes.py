19d6a8b919038d3c5177e1202a7261607776c402
Marcus Rosenow <2048345+Brickster@users.noreply.github.com>
2015-04-17 09:18:15 -0500

    Add --stat option
    Including --stat will cause changes to output a diffstat for each commit.

--- bin/commands/changes.py
+++ bin/commands/changes.py
@@ -2,10 +2,12 @@ from subprocess import call, check_output
 from utils.messages import info
 
 
-def changes(branch, count=False):
+def changes(branch, count=False, stat=False):
     """Print the changes between a given branch and HEAD"""
 
-    command = ['git', 'log', '--oneline', '{}..HEAD'.format(branch)]
+    command = ['git', 'log', '--oneline']
+    command = command if not stat or count else command + ['--stat']
+    command += ['{}..HEAD'.format(branch)]
     if count:
         log = check_output(command)
         log = log.splitlines()
