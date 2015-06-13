565bb0d9db9ab845d7d3cfcae8a713356587d14c
Marcus Rosenow <2048345+Brickster@users.noreply.github.com>
2015-06-13 11:21:23 -0500

    Remove FlagAsValue action

--- bin/utils/parse_actions.py
+++ bin/utils/parse_actions.py
@@ -1,13 +1,6 @@
 import argparse
 
 
-def flag_as_value(value):
-    class FlagAsValue(argparse.Action):
-        def __call__(self, parser, namespace, values, option_string=None):
-            setattr(namespace, self.dest, value)
-    return FlagAsValue
-
-
 def multi_set(dest1, value1):
     class MultiSet(argparse.Action):
         def __call__(self, parser, namespace, values, option_string=None):
