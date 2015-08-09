1425ab869e45822b9f1ce32f30918d5c7b2c5196
Marcus Rosenow <2048345+Brickster@users.noreply.github.com>
2015-08-09 17:24:56 -0500

    Add --color|--no-color option

--- bin/commands/changes.py
+++ bin/commands/changes.py
@@ -1,16 +1,24 @@
 """List the commits between this branch and another."""
 
+import sys
+
 from subprocess import call, check_output
 from utils.messages import info
 
 
-def changes(branch, details=None):
+def changes(branch, details=None, color_when='auto'):
     """Print the changes between a given branch and HEAD"""
 
+    color_when = color_when.lower()
+    if color_when == 'never' or (color_when == 'auto' and not sys.stdout.isatty()):
+        color_when = "never"
+    elif color_when == 'auto' and sys.stdout.isatty():
+        color_when = 'always'
+
     if details == "diff":
-        call(['git', 'diff', branch, 'HEAD'])
+        call(['git', 'diff', '--color={}'.format(color_when), branch, 'HEAD'])
     elif details == "stat":
-        call(['git', 'diff', '--stat', branch, 'HEAD'])
+        call(['git', 'diff', '--color={}'.format(color_when), '--stat', branch, 'HEAD'])
     else:
         command = ['git', 'log', '--oneline', '{}..HEAD'.format(branch)]
         if details == "count":
@@ -18,4 +26,5 @@ def changes(branch, details=None):
             log = log.splitlines()
             info(len(log))
         else:
+            command += ['--color={}'.format(color_when)]
             call(command)
