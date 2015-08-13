e41f754ad83a066f2fe2adce05ccd611bda29c86
Marcus Rosenow <2048345+Brickster@users.noreply.github.com>
2015-08-12 19:17:53 -0500

    Refactor error printing
    Errors are now printed to stderr rather than stdout.

--- bin/utils/messages.py
+++ bin/utils/messages.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import sys
 
 
@@ -7,12 +9,12 @@ def error(message, exit=True):
     assert isinstance(message, str), "message must be a str"
     assert isinstance(exit, bool), "exit must be a bool"
 
-    print "error:", message
+    print("error:", message, file=sys.stderr)
     if exit:
-        sys.exit(2)
+        sys.exit(1)
 
 
 def info(message):
     """Print a simple info message"""
 
-    print message
+    print(message)
