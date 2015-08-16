48ab35e2b2e2294a553daa8f75ff9ecf7a00f452
Marcus Rosenow <Brickstertwo@users.noreply.github.com>
2015-08-15 20:53:54 -0500

    Fix status title
    The new way of determining branch information or status' title didn't include commits from remote branch nor properly describe detached HEAD cases.

--- bin/commands/stateextensions/status.py
+++ bin/commands/stateextensions/status.py
@@ -1,14 +1,37 @@
-import re
 from ast import literal_eval
 from subprocess import call, check_output, Popen, PIPE
-
-from .. import upstream
 
 
 class Colors:
     green = '\x1B[0;32m'
     red='\x1B[0;31m'
     no_color = '\x1B[0m'
+
+
+def _set_color_status(show_color):
+
+    # make sure status will output ANSI codes
+    # this must be done using config since status has no --color option
+    status_color = Popen(['git', 'config', '--local', 'color.status'], stdout=PIPE, stderr=PIPE)
+    color_status, status_color_err = status_color.communicate()
+    color_status = color_status.rstrip()  # strip the newline
+    call(['git', 'config', 'color.status', show_color])
+
+    return color_status
+
+
+def _reset_color_status(color_status):
+
+    # reset color.status to its original setting
+    if color_status == '':
+        call(['git', 'config', '--unset', 'color.status'])
+
+        # unset may leave an empty section, remove it if it is
+        section_count = literal_eval(check_output(['git', 'settings', 'list', '--local', '--count', 'color']))
+        if not section_count:
+            call(['git', 'config', '--remove-section', 'color'])
+    else:
+        call(['git', 'config', 'color.status', color_status])
 
 
 def title(**kwargs):
@@ -23,24 +46,10 @@ def title(**kwargs):
     if new_repository:
         title = 'status {no_color}({green}master{no_color})'.format(no_color=Colors.no_color, green=Colors.green)
     else:
-        branches = check_output(('git', 'branch')).splitlines()
-        branch = [m.group(1) for branch in branches for m in [re.match(r'\* (.*)', branch)] if m][0]
-        upstream_output = upstream.upstream(True)
-
-        if upstream_output:
-            title = 'status{no_color} ({green}{branch}{no_color}...{red}{remote}{no_color})'.format(
-                no_color=Colors.no_color,
-                green=Colors.green,
-                red=Colors.red,
-                branch=branch,
-                remote=upstream_output
-            )
-        else:
-            title = 'status{no_color} ({green}{branch}{no_color})'.format(
-                no_color=Colors.no_color,
-                green=Colors.green,
-                branch=branch
-            )
+        original_color_status = _set_color_status(show_color)
+        title = check_output('git status --branch --short'.split()).splitlines()[0].lstrip('# ')
+        title = 'status{} ({})'.format(Colors.no_color, title)
+        _reset_color_status(original_color_status)
 
     return title
 
@@ -49,13 +58,7 @@ def get(**kwargs):
 
     new_repository = kwargs.get('new_repository', False)
     show_color = kwargs.get('show_color', 'always')
-
-    # make sure status will output ANSI codes
-    # this must be done using config since status has no --color option
-    status_color = Popen(['git', 'config', '--local', 'color.status'], stdout=PIPE, stderr=PIPE)
-    status_color_out, status_color_err = status_color.communicate()
-    status_color_out = status_color_out.rstrip()  # strip the newline
-    call(['git', 'config', 'color.status', show_color])
+    original_color_status = _set_color_status(show_color)
 
     if new_repository:
         # check if status is empty
@@ -65,15 +68,6 @@ def get(**kwargs):
     else:
         status_output = check_output(['git', 'status', '--short', '--untracked-files=all'])
 
-    # reset color.status to its original setting
-    if status_color_out == '':
-        call(['git', 'config', '--unset', 'color.status'])
-
-        # unset may leave an empty section, remove it if it is
-        section_count = literal_eval(check_output(['git', 'settings', 'list', '--local', '--count', 'color']))
-        if not section_count:
-            call(['git', 'config', '--remove-section', 'color'])
-    else:
-        call(['git', 'config', 'color.status', status_color_out])
+    _reset_color_status(original_color_status)
 
     return status_output
