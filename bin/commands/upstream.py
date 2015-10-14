af1b247fe967ca17f2bc9d58195870b68f3efba9
Marcus Rosenow <2048345+Brickster@users.noreply.github.com>
2015-10-13 19:36:05 -0500

    Add branch option to upstream
    Not including one defaults to the old behavior of using the current
    branch.
    
    Fixes #26

--- bin/commands/upstream.py
+++ bin/commands/upstream.py
@@ -9,22 +9,23 @@ _MERGE_CONFIG = 'git config --local branch.{}.merge'
 _REMOTE_CONFIG = 'git config --local branch.{}.remote'
 
 
-def upstream(include_remote=False):
+def upstream(branch=None, include_remote=False):
     """Get the upstream branch of the current branch."""
 
     if not directories.is_git_repository():
         error('{0!r} not a git repository'.format(os.getcwd()))
 
-    branch_name = check_output('git rev-parse --abbrev-ref HEAD'.split()).splitlines()[0]
+    if not branch:
+        branch = check_output('git rev-parse --abbrev-ref HEAD'.split()).strip()
 
     # get remote branch name
-    proc = Popen(_MERGE_CONFIG.format(branch_name).split(), stdout=PIPE)
+    proc = Popen(_MERGE_CONFIG.format(branch).split(), stdout=PIPE)
     upstream_info = proc.communicate()[0].strip()
     upstream_info = upstream_info.rsplit('/', 1)[-1]
 
     # optionally, get remote name
     if upstream_info and include_remote:
-        remote_name = check_output(_REMOTE_CONFIG.format(branch_name).split()).strip()
+        remote_name = check_output(_REMOTE_CONFIG.format(branch).split()).strip()
         upstream_info = remote_name + '/' + upstream_info
 
     return upstream_info
