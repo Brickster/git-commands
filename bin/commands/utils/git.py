c5e324bfc1b9b58b9894bde56a4721a1e7d00d75
Marcus Rosenow <Brickstertwo@users.noreply.github.com>
2016-03-29 18:41:13 -0500

    Fix docstrings

--- bin/commands/utils/git.py
+++ bin/commands/utils/git.py
@@ -8,7 +8,8 @@ def is_valid_reference(reference):
 
     :param str reference: name of the reference to validate
 
-    :return bool: whether or not the reference is valid"""
+    :return bool: whether or not the reference is valid
+    """
 
     show_ref_proc = Popen(['git', 'show-ref', '--quiet', reference])
     show_ref_proc.communicate()
@@ -18,6 +19,7 @@ def is_valid_reference(reference):
 def current_branch():
     """Returns the current branch.
 
-    :return str or unicode: the name of the current branch"""
+    :return str or unicode: the name of the current branch
+    """
 
     return check_output(('git', 'rev-parse', '--abbrev-ref', 'HEAD')).strip()
