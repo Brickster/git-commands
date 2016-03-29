1c55ebe95a077a621d0fa54ff4f6e3de0dc5185e
Marcus Rosenow <Brickstertwo@users.noreply.github.com>
2016-03-29 18:49:04 -0500

    Add method to check if an object is a commit

--- bin/commands/utils/git.py
+++ bin/commands/utils/git.py
@@ -1,6 +1,6 @@
 """A collection of common git actions."""
 
-from subprocess import check_output, Popen
+from subprocess import check_output, PIPE, Popen, STDOUT
 
 
 def is_valid_reference(reference):
@@ -14,6 +14,19 @@ def is_valid_reference(reference):
     show_ref_proc = Popen(['git', 'show-ref', '--quiet', reference])
     show_ref_proc.communicate()
     return not show_ref_proc.returncode
+
+
+def is_commit(object):
+    """Determines if an object is a commit.
+
+    :param object: a git object
+
+    :return bool: whether or not the object is a commit object
+    """
+
+    cat_file_proc = Popen(['git', 'cat-file', '-t', object], stdout=PIPE, stderr=STDOUT)
+    object_type = cat_file_proc.communicate()[0].strip()
+    return not cat_file_proc.returncode and object_type == 'commit'
 
 
 def current_branch():
