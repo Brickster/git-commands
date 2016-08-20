cb163d2a6db14e527341f94c62540be505d5e912
Marcus Rosenow <Brickstertwo@users.noreply.github.com>
2016-08-20 16:08:14 -0500

    Add upstream unit tests

--- bin/commands/upstream.py
+++ bin/commands/upstream.py
@@ -1,10 +1,10 @@
 """Get the current upstream branch."""
 
 import os
-from subprocess import check_output, PIPE, Popen
+import subprocess
+from subprocess import PIPE
 
-from utils import directories, git
-from utils.messages import error
+from utils import directories, git, messages
 
 _MERGE_CONFIG = 'git config --local branch.{}.merge'
 _REMOTE_CONFIG = 'git config --local branch.{}.remote'
@@ -20,21 +20,21 @@ def upstream(branch=None, include_remote=False):
     """
 
     if not directories.is_git_repository():
-        error('{0!r} not a git repository'.format(os.getcwd()))
+        messages.error('{0!r} not a git repository'.format(os.getcwd()))
 
     if not branch:
         branch = git.current_branch()
     elif not git.is_valid_reference(branch):
-        error('{0!r} is not a valid branch'.format(branch))
+        messages.error('{0!r} is not a valid branch'.format(branch))
 
     # get remote branch name
-    proc = Popen(_MERGE_CONFIG.format(branch).split(), stdout=PIPE)
+    proc = subprocess.Popen(_MERGE_CONFIG.format(branch).split(), stdout=PIPE)
     upstream_info = proc.communicate()[0].strip()
     upstream_info = upstream_info.rsplit('/', 1)[-1]
 
     # optionally, get remote name
     if upstream_info and include_remote:
-        remote_name = check_output(_REMOTE_CONFIG.format(branch).split()).strip()
+        remote_name = subprocess.check_output(_REMOTE_CONFIG.format(branch).split()).strip()
         upstream_info = remote_name + '/' + upstream_info
 
     return upstream_info
