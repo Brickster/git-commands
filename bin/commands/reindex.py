cb20c1ea2f24e90e98bc5266a2e1dc68dcf4561b
Marcus Rosenow <Brickstertwo@users.noreply.github.com>
2016-08-21 14:28:34 -0500

    Add reindex unit tests

--- bin/commands/reindex.py
+++ bin/commands/reindex.py
@@ -1,18 +1,17 @@
 """Re-add already indexed files to the index."""
 
 import os
-from subprocess import call, check_output
+import subprocess
 
-from utils import directories, git
-from utils.messages import error
+from utils import directories, git, messages
 
 
 def reindex():
 
     if not directories.is_git_repository():
-        error('{0!r} not a git repository'.format(os.getcwd()))
+        messages.error('{0!r} not a git repository'.format(os.getcwd()))
 
-    indexed_files = check_output(['git', 'diff', '--name-only', '--cached']).splitlines()
+    indexed_files = subprocess.check_output(['git', 'diff', '--name-only', '--cached']).splitlines()
     indexed_files = [f for f in indexed_files if f not in git.deleted_files()]
     if indexed_files:
-        call(['git', 'add', '--'] + indexed_files)
+        subprocess.call(['git', 'add', '--'] + indexed_files)
---
+++ tests/unit/test_reindex.py
@@ -0,0 +1,94 @@
+import mock
+import unittest
+
+import utils
+from bin.commands import reindex
+
+
+class TestReindex(unittest.TestCase):
+
+    @mock.patch('bin.commands.utils.directories.is_git_repository', return_value=True)
+    @mock.patch('subprocess.check_output')
+    @mock.patch('bin.commands.utils.git.deleted_files', return_value=['file3'])
+    @mock.patch('subprocess.call')
+    def test_reindex_noneDeleted(self, mock_call, mock_deletedfiles, mock_checkoutput, mock_isgitrepository):
+
+        # setup
+        files = ['file1', 'file2']
+        mock_checkoutput.return_value = '\n'.join(files) + '\n'
+
+        # when
+        reindex.reindex()
+
+        # then
+        mock_isgitrepository.assert_called_once_with()
+        mock_checkoutput.assert_called_once_with('git diff --name-only --cached'.split())
+        mock_call.assert_called_once_with(['git', 'add', '--'] + files)
+
+    @mock.patch('bin.commands.utils.directories.is_git_repository', return_value=True)
+    @mock.patch('subprocess.check_output')
+    @mock.patch('bin.commands.utils.git.deleted_files')
+    @mock.patch('subprocess.call')
+    def test_reindex_someDeleted(self, mock_call, mock_deletedfiles, mock_checkoutput, mock_isgitrepository):
+
+        # setup
+        files = ['file1', 'file2', 'file3']
+        mock_checkoutput.return_value = '\n'.join(files) + '\n'
+        mock_deletedfiles.return_value = ['file2']
+
+        # when
+        reindex.reindex()
+
+        # then
+        mock_isgitrepository.assert_called_once_with()
+        mock_checkoutput.assert_called_once_with('git diff --name-only --cached'.split())
+        mock_call.assert_called_once_with(['git', 'add', '--', 'file1', 'file3'])
+
+    @mock.patch('bin.commands.utils.directories.is_git_repository', return_value=True)
+    @mock.patch('subprocess.check_output')
+    @mock.patch('bin.commands.utils.git.deleted_files')
+    @mock.patch('subprocess.call')
+    def test_reindex_allDeleted(self, mock_call, mock_deletedfiles, mock_checkoutput, mock_isgitrepository):
+
+        # setup
+        files = ['file1', 'file2']
+        mock_checkoutput.return_value = '\n'.join(files) + '\n'
+        mock_deletedfiles.return_value = files
+
+        # when
+        reindex.reindex()
+
+        # then
+        mock_isgitrepository.assert_called_once_with()
+        mock_checkoutput.assert_called_once_with('git diff --name-only --cached'.split())
+        mock_call.assert_not_called()
+
+    @mock.patch('bin.commands.utils.directories.is_git_repository', return_value=True)
+    @mock.patch('subprocess.check_output', return_value = '')
+    @mock.patch('subprocess.call')
+    def test_reindex_noFilesToIndex(self, mock_call, mock_checkoutput, mock_isgitrepository):
+
+        # when
+        reindex.reindex()
+
+        # then
+        mock_isgitrepository.assert_called_once_with()
+        mock_checkoutput.assert_called_once_with('git diff --name-only --cached'.split())
+        mock_call.assert_not_called()
+
+    @mock.patch('bin.commands.utils.directories.is_git_repository', return_value=False)
+    @mock.patch('bin.commands.utils.messages.error', side_effect=utils.and_exit)
+    @mock.patch('os.getcwd', return_value='/working/dir')
+    def test_reindex_notAGitRepository(self, mock_getcwd, mock_error, mock_isgitrepository):
+
+        # when
+        try:
+            reindex.reindex()
+            self.fail('expected to exit but did not')
+        except SystemExit:
+            pass
+
+        # then
+        mock_isgitrepository.assert_called_once_with()
+        mock_error.assert_called_once_with("'/working/dir' not a git repository")
+        mock_getcwd.assert_called_once_with()
