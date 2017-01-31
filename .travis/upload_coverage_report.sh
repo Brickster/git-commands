6adf24bdf164d897b82061791514c5a2f7a19416
Marcus Rosenow <Brickstertwo@users.noreply.github.com>
2017-01-30 18:39:45 -0600

    Upload coverage reports with commit

--- .travis/upload_coverage_report.sh
+++ .travis/upload_coverage_report.sh
@@ -3,7 +3,7 @@ set -ev
 
 # upload coverage report for pushes to master only
 # NOTE: secure environment variables aren't available for PRs from other repositories
-if [[ ! -z "$CODACY_PROJECT_TOKEN" ]] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+if [[ ! -z "$CODACY_PROJECT_TOKEN" ]]; then
     coverage xml --include=bin/* --omit=*/__init__.py -o coverage.xml
-    python-codacy-coverage -r coverage.xml
+    python-codacy-coverage --commit "$TRAVIS_COMMIT" --report coverage.xml
 fi
