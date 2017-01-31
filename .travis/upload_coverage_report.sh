6e791838f2f68ea1bddeb49adbe52c4557b5f83e
Marcus Rosenow <Brickstertwo@users.noreply.github.com>
2017-01-30 20:24:21 -0600

    Remove commit

--- .travis/upload_coverage_report.sh
+++ .travis/upload_coverage_report.sh
@@ -5,5 +5,7 @@ set -ev
 # NOTE: secure environment variables aren't available for PRs from other repositories
 if [[ ! -z "$CODACY_PROJECT_TOKEN" ]]; then
     coverage xml --include=bin/* --omit=*/__init__.py -o coverage.xml
-    python-codacy-coverage --commit "$TRAVIS_COMMIT" --report coverage.xml
+    # python-codacy-coverage --commit "$TRAVIS_COMMIT" --report coverage.xml
+    echo "$TRAVIS_COMMIT"
+    python-codacy-coverage --report coverage.xml
 fi
