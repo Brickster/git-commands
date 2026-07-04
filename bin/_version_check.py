96950b8a3b1e0570cc96a5b11ce206c95f7d8489
Marcus Rosenow <2048345+Brickster@users.noreply.github.com>
2026-07-04 09:23:02 -0500

    fix: add Python 3.10 version check to all entry points

---
+++ bin/_version_check.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+
+def check(min_version=(3, 10)):
+    if sys.version_info < min_version:
+        cmd = os.path.basename(sys.argv[0]).replace('git-', 'git ')
+        version = f'{sys.version_info.major}.{sys.version_info.minor}'
+        required = '.'.join(str(v) for v in min_version)
+        sys.exit(f'error: {cmd} requires Python {required} or later (found {version})')
